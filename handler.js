c4861a741530348c4e0d5f2ee39e804922457f4e
Cristian Olivares <cristianolivares@MacBook-Pro-de-Cristian-5.local>
2023-01-21 01:41:13 -0300

    fix: add environment variables

--- handler.js
+++ handler.js
@@ -1,42 +1,62 @@
-
-/*module.exports.handler = async (event, context, callback) => {
-  try {
-    
-    console.log('test successfully');
-
-    return callback(undefined, {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: 'test successfully',
-        }
-      ),
-    });
-  } catch (error) {
-    console.log(error);
-    return callback(undefined, {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: 'Error',
-        }
-      ),
-    });
-  }
-};
-*/
 const MongoClient = require("mongodb").MongoClient;
-//import { v4 as uuid } from 'uuid';
-const credencialsMongo = process.env.mongo_connection
-//const database = 'test';
-//const credencialsMongo = 'mongodb://sudev:dev%4031415@3.137.164.24:27017';
+const { v4: uuidv4 } = require('uuid');
+const credencialsMongo = process.env.mongo_connection;
 const database = 'test';
 
 module.exports.handler = async (event, context, callback) => {
 
-
     await MongoClient.connect(credencialsMongo).then(async (client) => {
-        console.log('conectado a mongo');
+
+      const connect = await client.db(database);
+      const collection = await connect.collection("orders");
+
+      const order = event.detail;
+
+      const FormatOrdenAdmin = {
+          "id": order.id || uuidv4(),
+          "customer": order.customer,
+          "productsOrder": order.productsOrder.map((product) => {
+              return {
+                  ...product,
+                  prescription: {
+                      state: "",
+                      file: product.prescription || "",
+                      validation: {
+                          rut: "",
+                          comments: ""
+                      }
+                  }
+              } 
+          }),
+          delivery: { ...order.delivery, 
+                      "provider": {
+                          "provider": "",
+                          "orderTransport": "",
+                          "urlLabel": ""
+                      }},
+          
+          
+          "payment": order.payment,
+          "resumeOrder": order.resumeOrder,
+          "statusOrder": order.statusOrder || "VALIDANDO_RECETA",
+          "createdAt": order.createdAt || new Date(),
+          "billing": {
+              "type": "",
+              "number": "",
+              "emitter": "",
+              "urlBilling": ""
+          },
+      }
+
+
+      console.log('--- order received --');
+      console.log(JSON.stringify(FormatOrdenAdmin));
+
+      await collection.insertOne(FormatOrdenAdmin);
+
+      console.log('--- order save sucessfully --');
+
+
     });
 
     return {
