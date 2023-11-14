6046f8b03693f4565b769fe4dc65aefb2a603c3b
Matias Martinez <matias.martinez@farmaloop.cl>
2023-11-14 02:12:18 -0300

    feat: cotizacion

--- handler.js
+++ handler.js
@@ -1,31 +1,31 @@
-const { MongoClient } = require("mongodb");
-const { v4: uuidv4 } = require("uuid");
+const { MongoClient } = require('mongodb');
+const { v4: uuidv4 } = require('uuid');
 const credencialsMongo = process.env.MONGO_CONECTION;
 const database = process.env.MONGO_DATABASE;
 
-module.exports.handler = async (event, context, callback) => {
+module.exports.handler = async (event, _context, _callback) => {
   const client = new MongoClient(credencialsMongo);
 
   try {
     await client.connect();
     const connect = client.db(database);
 
-    const collection = await connect.collection("orders");
+    const collection = await connect.collection('orders');
 
     const order = event.detail;
 
-    const FormatOrdenAdmin = {
+    const formatOrdenAdmin = {
       id: order.id || uuidv4(),
       customer: order.customer,
-      productsOrder: order.productsOrder.map(product => {
+      productsOrder: order.productsOrder.map((product) => {
         return {
           ...product,
           prescription: {
-            state: "",
-            file: product.prescription || "",
+            state: '',
+            file: product.prescription || '',
             validation: {
-              rut: "",
-              comments: "",
+              rut: '',
+              comments: '',
             },
           },
         };
@@ -33,37 +33,41 @@ module.exports.handler = async (event, context, callback) => {
       delivery: {
         ...order.delivery,
         provider: {
-          provider: "",
-          orderTransport: "",
-          urlLabel: "",
+          provider: '',
+          orderTransport: '',
+          urlLabel: '',
         },
       },
 
       payment: order.payment,
       resumeOrder: order.resumeOrder,
-      statusOrder: order.statusOrder || "VALIDANDO_RECETA",
+      statusOrder: order.statusOrder || 'VALIDANDO_RECETA',
       createdAt: order.createdAt || new Date(),
       billing: {
-        type: "",
-        number: "",
-        emitter: "",
-        urlBilling: "",
+        type: '',
+        number: '',
+        emitter: '',
+        urlBilling: '',
       },
     };
 
-    console.log("--- order received --");
-    console.log(JSON.stringify(FormatOrdenAdmin));
+    if (order.cotizacion) {
+      formatOrdenAdmin.cotizacion = order.cotizacion;
+    }
 
-    await collection.insertOne(FormatOrdenAdmin);
+    console.log('--- order received --');
+    console.log(JSON.stringify(formatOrdenAdmin));
 
-    console.log("--- order save sucessfully --");
+    await collection.insertOne(formatOrdenAdmin);
+
+    console.log('--- order save sucessfully --');
   } finally {
     await client.close();
-    console.log("finalizado");
+    console.log('finalizado');
   }
 
   return {
-    statusCode: "200",
+    statusCode: '200',
     body: JSON.stringify(event),
   };
 };
