4a7fdb88de6daadcbb9b4ef98a28a22865c1044f
Matias Martinez <matias.martinez@farmaloop.cl>
2024-02-05 23:55:37 -0300

    feat: node 20, typescript y tracking

---
+++ src/handler.ts
@@ -0,0 +1,60 @@
+import { OrderFromEcommerce } from './interface';
+import { getMongoClient } from './mongo_connection';
+
+const mongoClient = getMongoClient();
+
+export const handler = async (event: any, _context: any, _callback: any) => {
+  try {
+    const order: OrderFromEcommerce = event.detail;
+
+    console.log('--- Orden de Ecommerce: ', JSON.stringify(order, null, 2));
+
+    const db_connection = mongoClient.db(process.env.MONGO_DATA_BASE);
+
+    try {
+      await db_connection.admin().ping();
+      console.log('--- Conexión a MongoDB es válida.');
+    } catch (error) {
+      console.log('--- Intentando nueva conexión a MongoDB.');
+      await mongoClient.connect();
+    }
+
+    const orders_collection = db_connection.collection('orders');
+
+    const new_order: any = {
+      id: order.id,
+      customer: order.customer,
+      productsOrder: order.productsOrder.map((product) => {
+        return {
+          ...product,
+          prescription: { state: '', file: product.prescription ?? '', validation: { rut: '', comments: '' } },
+        };
+      }),
+      delivery: { ...order.delivery, provider: { provider: '', orderTransport: '', urlLabel: '' } },
+      payment: order.payment,
+      resumeOrder: order.resumeOrder,
+      statusOrder: order.statusOrder,
+      createdAt: new Date(),
+      billing: { type: '', number: '', emitter: '', urlBilling: '' },
+      tracking: [{ date: new Date(), responsible: 'eCommerce', toStatus: order.statusOrder }],
+    };
+
+    if (order.cotizacion) {
+      new_order.cotizacion = order.cotizacion;
+    }
+
+    console.log('Se va a crear orden: ', JSON.stringify(new_order, null, 2));
+
+    await orders_collection.insertOne(new_order);
+
+    return {
+      statusCode: '200',
+      body: JSON.stringify(event),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(event),
+    };
+  }
+};
