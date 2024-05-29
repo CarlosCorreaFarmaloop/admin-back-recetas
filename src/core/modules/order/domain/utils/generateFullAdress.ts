49af829091d92b68223a48d5fe5ed36609e6f59a
Carlos <correamillancarlos@gmail.com>
2024-05-29 12:26:39 -0400

    fix address

--- src/core/modules/order/domain/utils/generateFullAdress.ts
+++ src/core/modules/order/domain/utils/generateFullAdress.ts
@@ -3,10 +3,10 @@ import { DeliveryAddress } from '../order.entity';
 export const generateFullAdress = (delivery_address: DeliveryAddress) => {
   const hasDpto = delivery_address.dpto ? `${delivery_address.homeType}: ${delivery_address.dpto} ` : '';
 
-  const streetNumber = delivery_address.streetNumber ?? '';
+  const streetNumber = `${delivery_address.streetNumber} ` ?? '';
 
   return (
-    `${delivery_address.streetName} ${streetNumber}, ${hasDpto}${delivery_address.comuna}, ${delivery_address.region}` ??
+    `${delivery_address.streetName}${streetNumber}, ${hasDpto}${delivery_address.comuna}, ${delivery_address.region}` ??
     ''
   );
 };
