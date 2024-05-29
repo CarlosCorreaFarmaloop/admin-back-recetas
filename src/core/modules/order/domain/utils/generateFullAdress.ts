b89245134c6d37d4b9868248efd64f7270155a7f
Carlos <correamillancarlos@gmail.com>
2024-05-29 16:38:41 -0400

    fix: full addres

--- src/core/modules/order/domain/utils/generateFullAdress.ts
+++ src/core/modules/order/domain/utils/generateFullAdress.ts
@@ -3,7 +3,7 @@ import { DeliveryAddress } from '../order.entity';
 export const generateFullAdress = (delivery_address: DeliveryAddress) => {
   const hasDpto = delivery_address.dpto ? `${delivery_address.homeType}: ${delivery_address.dpto} ` : '';
 
-  const streetNumber = `${delivery_address.streetNumber} ` ?? '';
+  const streetNumber = ` ${delivery_address.streetNumber}` ?? '';
 
   return (
     `${delivery_address.streetName}${streetNumber}, ${hasDpto}${delivery_address.comuna}, ${delivery_address.region}` ??
