544bd218a0208471829595a66c652a21ad8c6b14
Matias Martinez <matias.martinez@farmaloop.cl>
2024-06-07 15:54:34 -0400

    .

--- src/infra/dto/subscription/create.dto.ts
+++ src/infra/dto/subscription/create.dto.ts
@@ -1,6 +1,10 @@
 import Joi from 'joi';
 
 const createSchema = Joi.object({
+  id: Joi.string().required(),
+  createdAt: Joi.number().required(),
+  updatedAt: Joi.number().required(),
+
   customer: Joi.string().required(),
   delivery: Joi.object({
     comuna: Joi.string().required(),
@@ -57,11 +61,119 @@ const createSchema = Joi.object({
       }).unknown()
     )
     .required(),
+
+  paymentMethods: Joi.array()
+    .items(
+      Joi.object({
+        cardNumber: Joi.string().required(),
+        cardType: Joi.string().required(),
+        externalStatus: Joi.string().required(),
+        recordDate: Joi.number().required(),
+        secret: Joi.string().required(),
+        status: Joi.number().required(),
+        token: Joi.string().required(),
+        transactionCode: Joi.string().required(),
+      })
+    )
+    .required(),
   shipment: Joi.object({
     intervalMonth: Joi.number().required(),
     dayOfMonth: Joi.number().required(),
     numberOfShipments: Joi.number().required(),
+
+    startDate: Joi.number().required(),
+    endDate: Joi.number().required(),
+
+    quantityShipped: Joi.number().required(),
+
+    shipmentSchedule: Joi.array()
+      .items(
+        Joi.object({
+          id: Joi.string().required(),
+          paymentDate: Joi.number().required(),
+          nextPaymentDate: Joi.number().required(),
+          shipmentDate: Joi.number().required(),
+          paymentStatus: Joi.string().required(),
+
+          orderId: Joi.string().required().allow(''),
+          orderStatus: Joi.string().required(),
+
+          numberOfAttempts: Joi.number().required(),
+          maxAttempts: Joi.number().required(),
+          attempts: Joi.array()
+            .items(
+              Joi.object({
+                cardNumber: Joi.string().required(),
+                externalCode: Joi.string().required(),
+                externalMessage: Joi.string().required(),
+                externalStatus: Joi.string().required(),
+                paymentMethod: Joi.string().required(),
+                status: Joi.string().required(),
+                transactionDate: Joi.number().required(),
+              })
+            )
+            .required(),
+        })
+      )
+      .required(),
   }),
+
+  nextPaymentDate: Joi.number().required(),
+  nextShipmentDate: Joi.number().required(),
+  currentPaymentId: Joi.string().required(),
+  currentShipmentId: Joi.string().required(),
+  generalStatus: Joi.string().required(),
+  paymentStatus: Joi.string().required(),
+  progressStatus: Joi.string().required(),
+
+  trackingGeneralStatus: Joi.array()
+    .items(
+      Joi.object({
+        date: Joi.number().required(),
+        responsible: Joi.string().required(),
+        observation: Joi.string().required().allow(''),
+        status: Joi.string().required(),
+      })
+    )
+    .required(),
+  trackingPaymentStatus: Joi.array()
+    .items(
+      Joi.object({
+        date: Joi.number().required(),
+        responsible: Joi.string().required(),
+        observation: Joi.string().required().allow(''),
+        status: Joi.string().required(),
+      })
+    )
+    .required(),
+  trackingProgressStatus: Joi.array()
+    .items(
+      Joi.object({
+        date: Joi.number().required(),
+        responsible: Joi.string().required(),
+        observation: Joi.string().required().allow(''),
+        status: Joi.string().required(),
+      })
+    )
+    .required(),
+
+  observations: Joi.array()
+    .items(
+      Joi.object({
+        date: Joi.number().required(),
+        comment: Joi.string().required(),
+        responsible: Joi.string().required(),
+      })
+    )
+    .required(),
+  resume: Joi.object({
+    deliveryPrice: Joi.number().required(),
+    discount: Joi.number().required(),
+    numberOfProducts: Joi.number().required(),
+    subtotal: Joi.number().required(),
+    total: Joi.number().required(),
+    totalWithoutDiscount: Joi.number().required(),
+  }).required(),
 });
 
 export function Create_Subscription_Dto(record: any) {
