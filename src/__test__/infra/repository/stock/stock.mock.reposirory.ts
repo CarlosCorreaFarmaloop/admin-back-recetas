6f69f4e2a0579359803f44dc3858fbe684bd7409
Matias Martinez <matias.martinez@farmaloop.cl>
2024-06-18 18:57:14 -0400

    .

--- src/__test__/infra/repository/stock/stock.mock.reposirory.ts
+++ src/__test__/infra/repository/stock/stock.mock.reposirory.ts
@@ -2,7 +2,7 @@ import { StockEntity } from '../../../../core/modules/stock/domain/stock.entity'
 import { StockRepository } from '../../../../core/modules/stock/domain/stock.repository';
 
 export class StockMockRepository implements StockRepository {
-  private stocks: StockEntity[] = [];
+  private readonly stocks: StockEntity[] = [];
 
   async getAllBySku(skus: string[]) {
     const currentStocks = this.stocks.filter((el) => skus.includes(el.sku));
